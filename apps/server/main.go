6f28190be3fc6cae10231e39835daacf29cfea4f
Sebastian009w <ia3266595@gmail.com>
2023-06-19 19:00:58 -0500

    fiber init #3

--- apps/server/main.go
+++ apps/server/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	PORT := os.Getenv("PORT")
+	port_number, err := strconv.Atoi(PORT)
+
+	if err != nil {
+		fmt.Println("Error PORT", err)
+		return
+	}
+
 	app := fiber.New()
 
-	app.Listen("")
+	app.Listen(fmt.Sprintf(":%d", port_number))
 }
