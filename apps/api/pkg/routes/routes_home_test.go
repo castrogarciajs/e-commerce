c0a442259d8bab7e8a301beed8323f0e42f4328b
Sebastian009w <ia3266595@gmail.com>
2023-06-23 23:29:24 -0500

    test route home

--- apps/api/pkg/routes/routes_home_test.go
+++ apps/api/pkg/routes/routes_home_test.go
@@ -1,1 +1,26 @@
 package routes
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
+)
+
+func TestHome(t *testing.T) {
+
+	t.Parallel()
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+
+		return c.SendString("Home")
+	})
+
+	res, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/", nil))
+
+	utils.AssertEqual(t, nil, err, "app.Test(req)")
+	utils.AssertEqual(t, 200, res.StatusCode, "Status Code")
+}
