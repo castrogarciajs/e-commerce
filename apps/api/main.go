0c4def348a02f5c95fb5b12f31b4dddb10977bd2
Sebastian009w <ia3266595@gmail.com>
2023-06-23 22:55:11 -0500

    route home

--- apps/api/main.go
+++ apps/api/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/sebastian009w/e-commerce/pkg/routes"
 )
 
 func main() {
 
 	app := fiber.New()
 
+	app.Get("/", routes.Home)
 	app.Listen(":3000")
 	fmt.Println("Server on port: 3000")
 }
