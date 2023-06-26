fd55556dccff082296742b2996defd22a5a18286
Sebastian009w <ia3266595@gmail.com>
2023-06-25 21:33:55 -0500

    boilerplate clear #12

--- apps/client/routes/index.tsx
+++ apps/client/routes/index.tsx
@@ -1,25 +1,16 @@
 import { Head } from "$fresh/runtime.ts";
 import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   const count = useSignal(3);
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>E - commerce</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter count={count} />
+        <h1>Hello Deno Fresh</h1>
+        <h3>{count}</h3>
       </div>
     </>
   );
